48f7c11117c5cec02f0f3d7c97e5f7063643ad51
Daniela Dell Acqua <ddacqua@gmail.com>
2023-05-31 11:49:06 -0300

    update

--- rick_and_morty-ok/rick_and_morty/src/componentes/About/About.jsx
+++ rick_and_morty-ok/rick_and_morty/src/componentes/About/About.jsx
@@ -1,28 +1,22 @@
+import React from 'react';
 import style from './About.module.css'
 import image from '../../image/dani.jpg';
-import React from 'react';
+
 
 const About = () => {
     return (
         <>
         <h2 className={style.title}>Daniela Dell Acqua - Henry Flex 2023</h2>
         
-        
         <div className={style['container']}>
-
-        
+            
         <a href="https://github.com/DaniDell/Integration_R_M" target="_blank" rel="noopener noreferrer">
         <p className={style['bio']}> She studied design at Buenos Aires University and UX/UI at Coderhouse. These are her first code lines, tap here to see the repo. </p>
-    </a>
+        </a>
 
- 
-        
+
         <a className={style['porfolio']} href="https://www.behance.net/dandellacqua">Visit her Behance</a>
-        <img 
-            src={image} 
-            alt="Dani" 
-            className={style.Img} 
-         /> 
+        <img src={image} alt="Daniela Dell Acqua" className={style.Img} /> 
         
         </div>  
           
